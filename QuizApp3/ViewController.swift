22abaf050682abd24e5c4e3d74b71d31860cfa98
池田享浩 <ikedatakahiro@rrcs-172-254-99-20.nyc.biz.rr.com>
2016-01-16 00:14:17 +0900

    Initial Commit

---
+++ QuizApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp3
+//
+//  Created by 池田享浩 on 2016/01/16.
+//  Copyright © 2016年 takahiro ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
